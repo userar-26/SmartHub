1fbadeafa370c0e13f0249c3b58af1a550c038bc
userar26 <roman26@gmail.com>
2025-10-08 22:57:20 +0300

    feat: Добавил библиотеку cJSON и настроил системы сборки
    Добавлена библиотека cJSON для обмена данными в формате JSON
    между всеми устройствами проекта.
    
    - Для hub_linux: библиотека подключена через CMakeLists.txt как
    подпроект в общей директории common.
    
    - Для node_arduino и node_esp32: библиотека подключена через
    platformio.ini с использованием файла-манифеста library.json в папке common.

---
+++ common/smarthub_protocol.h
@@ -0,0 +1,39 @@
+#ifndef SMARTHUB_PROTOCOL_H
+#define SMARTHUB_PROTOCOL_H
+
+// ================================================================
+// ==================== MQTT ТОПИКИ (только для ESP32) ============
+// ================================================================
+#define TOPIC_ESP32_TO_HUB "smarthub/esp32/data"
+#define TOPIC_HUB_TO_ESP32 "smarthub/esp32/command"
+
+
+// ================================================================
+// ==================== КЛЮЧИ JSON (Общие для всех) ===============
+// ================================================================
+// Используются во всех сообщениях
+#define KEY_DEVICE      "device"
+#define KEY_COMMAND     "command"
+#define KEY_STATE       "state"
+#define KEY_VALUES      "values"
+
+// Идентификаторы устройств
+#define DEVICE_DOOR             "door"
+#define DEVICE_ALARM            "alarm"
+#define DEVICE_MOTION_SENSOR    "motion_sensor"
+#define DEVICE_DHT_SENSOR       "dht_sensor"
+#define DEVICE_LIVING_ROOM_LIGHT "living_room_light"
+
+// Команды
+#define CMD_SET_STATE   "set_state"
+#define CMD_GET_DATA    "get_data"
+
+// Состояния
+#define STATE_OPEN      "open"
+#define STATE_CLOSE     "close"
+#define STATE_ON        "on"
+#define STATE_OFF       "off"
+#define STATE_DETECTED  "detected"
+#define STATE_CLEAR     "clear"
+
+#endif //SMARTHUB_PROTOCOL_H
