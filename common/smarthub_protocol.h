f203b28b295f3e8c00bef638445c386de952f3f1
userar26 <roman26@gmail.com>
2025-10-09 21:10:42 +0300

    feat: Были добавлены новые ключи и значение в smarthub_protocol.h
    Были добавлены специальные ключи и значения для этих ключей,
    чтобы устройства esp32 и arduino могли отправлять сообщения в
    формате json об успешном или не успешном выполнение команд

--- common/smarthub_protocol.h
+++ common/smarthub_protocol.h
@@ -2,20 +2,40 @@
 #define SMARTHUB_PROTOCOL_H
 
 // ================================================================
-// ==================== MQTT ТОПИКИ (только для ESP32) ============
+// ==================== MQTT ТОПИКИ ===============================
 // ================================================================
 #define TOPIC_ESP32_TO_HUB "smarthub/esp32/data"
 #define TOPIC_HUB_TO_ESP32 "smarthub/esp32/command"
 
 
 // ================================================================
-// ==================== КЛЮЧИ JSON (Общие для всех) ===============
+// ==================== КЛЮЧИ JSON ================================
 // ================================================================
 // Используются во всех сообщениях
+#define KEY_TYPE        "type"
 #define KEY_DEVICE      "device"
 #define KEY_COMMAND     "command"
 #define KEY_STATE       "state"
 #define KEY_VALUES      "values"
+
+// --- Ключи для ответов ---
+#define KEY_STATUS      "status"
+#define KEY_NEW_STATE   "new_state"
+
+// --- Ключи для событий ---
+#define KEY_EVENT       "event"
+
+// ================================================================
+// ==================== ЗНАЧЕНИЯ ==================================
+// ================================================================
+
+// --- Типы сообщений ---
+#define TYPE_RESPONSE   "response"
+#define TYPE_EVENT      "event"
+
+// --- Статусы ответа ---
+#define STATUS_SUCCESS  "success"
+#define STATUS_ERROR    "error"
 
 // Идентификаторы устройств
 #define DEVICE_DOOR             "door"
@@ -33,7 +53,7 @@
 #define STATE_CLOSE     "close"
 #define STATE_ON        "on"
 #define STATE_OFF       "off"
-#define STATE_DETECTED  "detected"
 #define STATE_CLEAR     "clear"
+#define EVENT_DETECTED  "detected"
 
 #endif //SMARTHUB_PROTOCOL_H
